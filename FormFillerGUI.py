207d7a17f9320e29e0d0ce35fa5bb8e5b3ef4c9f
Miguel Lopez Rivera <mlopezrivera@umass.edu>
2021-07-06 16:45:39 -0400

    Added dropbox for files

--- FormFillerGUI.py
+++ FormFillerGUI.py
@@ -3,3 +3,17 @@
 
 import PySimpleGUI as sg
 
+sg.theme("DarkAmber")
+
+layout = [[ sg.Combo(['choice1', 'choice2', 'choice3'])]]
+
+window = sg.Window("MFD FormFiller", layout)
+
+
+
+while True:
+  event, values = window.read()
+  if event == "Exit" or event == sg.WIN_CLOSED:
+    break
+
+window.close()
